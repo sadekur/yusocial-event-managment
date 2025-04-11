7e81eed503a566980e40b4f70a62ed3d0488f48d
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-04-11 11:30:33 +0600

    assets add

--- includes/Assets.php
+++ includes/Assets.php
@@ -14,24 +14,24 @@ class Assets {
     public function yem_frontend_assets() {
         wp_enqueue_script(
             'yem-frontend-script',
-            YUSOCIAL_EM_ASSETS . 'js/frontend.js',
+            YUSOCIAL_EM_ASSETS . '/js/frontend.js',
             ['jquery'],
-            filemtime(YUSOCIAL_EM_PATH . 'assets/js/frontend.js'),
+            filemtime(YUSOCIAL_EM_PATH . '/assets/js/frontend.js'),
             true
         );
+
         wp_localize_script('yem-frontend-script', 'YUSOCIAL_EM', [
-            'ajaxurl' => admin_url('admin-ajax.php'),
+            'ajaxurl'  => admin_url('admin-ajax.php'),
             'adminurl' => admin_url(),
-            'nonce'   => wp_create_nonce('yem-nonce'),
-            'error'   => __('Something went wrong', 'yusocial-event-managment'),
+            'nonce'    => wp_create_nonce('yem-nonce'),
+            'error'    => __('Something went wrong', 'yusocial-event-managment'),
         ]);
 
-        // Enqueue frontend styles
         wp_enqueue_style(
             'yem-frontend-style',
             YUSOCIAL_EM_ASSETS . '/css/frontend.css',
             [],
-            filemtime(YUSOCIAL_EM_PATH . 'assets/css/frontend.css')
+            filemtime(YUSOCIAL_EM_PATH . '/assets/css/frontend.css')
         );
     }
 
@@ -40,23 +40,23 @@ class Assets {
             'yem-admin-script',
             YUSOCIAL_EM_ASSETS . '/js/admin.js',
             ['jquery'],
-            filemtime(YUSOCIAL_EM_PATH . 'assets/js/admin.js'),
+            filemtime(YUSOCIAL_EM_PATH . '/assets/js/admin.js'),
             true
         );
 
         wp_localize_script('yem-admin-script', 'YUSOCIAL_EM', [
-            'nonce'    => wp_create_nonce( 'wp_rest' ),
+            'nonce'    => wp_create_nonce('wp_rest'),
             'adminurl' => admin_url(),
-            'ajaxurl'  => admin_url( 'admin-ajax.php' ),
-            'apiurl'   => untrailingslashit( rest_url( 'yusocial-event-managment/v1' ) ),
-            'error'    => __( 'Something went wrong', 'yusocial-event-managment' ),
+            'ajaxurl'  => admin_url('admin-ajax.php'),
+            'apiurl'   => untrailingslashit(rest_url('yusocial-event-managment/v1')),
+            'error'    => __('Something went wrong', 'yusocial-event-managment'),
         ]);
 
         wp_enqueue_style(
             'yem-admin-style',
             YUSOCIAL_EM_ASSETS . '/css/admin.css',
             [],
-            filemtime(YUSOCIAL_EM_PATH . 'assets/css/admin.css')
+            filemtime(YUSOCIAL_EM_PATH . '/assets/css/admin.css')
         );
     }
 }
